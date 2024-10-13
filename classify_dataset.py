ac0c4e58e44e37dc0257b0b1ed5267470854472f
Trần Thanh Nhân <134724987+normalman159@users.noreply.github.com>
2024-10-13 18:58:16 +0700

    init

--- classify_dataset.py
+++ classify_dataset.py
@@ -2,6 +2,7 @@ import cv2
 import mediapipe as mp
 import numpy as np
 import pickle
+import time
 
 model_dict = pickle.load(open('model.pickle', 'rb'))
 model = model_dict['model']
@@ -16,10 +17,28 @@ hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.5)
 
 expected_features = 84
 
+previous_char = None
+time_clear = 4
+timeWaitForNewChar = 1
+time_in = time.time()
+time_previous_char_first = 0
+prediction_char = None
+
 def hot_key_break():
     if cv2.waitKey(10) & 0xFF == 27:  # Press 'Esc' to exit
         return True
     return False
+
+def showPredictionPhrase() :
+    phrase_text = ''.join(predicted_phrase)
+    cv2.rectangle(frame_rgb_flip, (10, 10), (10 + len(phrase_text) * 20, 50), (0, 0, 0), -1)
+    cv2.putText(frame_rgb_flip, phrase_text, (15, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+def deleteChar() :
+    if len(predicted_phrase) > 0:
+        predicted_phrase.pop()
+
+predicted_phrase = []
 
 
 while True:
@@ -40,6 +59,8 @@ while True:
     results = hands.process(frame_rgb_flip)
 
     if results.multi_hand_landmarks:
+        time_in = time.time()
+
         for hand_landmarks in results.multi_hand_landmarks:
             mp_drawing.draw_landmarks(frame_rgb_flip, hand_landmarks, mp_hands.HAND_CONNECTIONS, 
                                       mp_drawing_styles.get_default_hand_landmarks_style(), 
@@ -72,10 +93,39 @@ while True:
         prediction_char = model.predict([np.array(data_aux)])
         if len(prediction_char) > 0:
             prediction_char = prediction_char[0]
- 
+
+        # Check if the predicted character is different from the previous one
+        if ((previous_char != prediction_char) or (previous_char is None)) :   
+            
+            time_current_char = time.time()
+            print(time_current_char - time_previous_char_first)
+            if (time_current_char - time_previous_char_first) > timeWaitForNewChar:
+                previous_char = prediction_char
+                if (prediction_char == 'space') : prediction_char = ' ' #Check if the prediction_char is space
+
+                if (prediction_char == 'del') : #Check if the prediction_char is delete
+                    deleteChar()
+                else : predicted_phrase.append(prediction_char)
+          
         # Display the predicted character on the frame
         cv2.putText(frame_rgb_flip, prediction_char, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-            
+
+        # Display the predicted phrase in a rectangle
+        print(predicted_phrase)
+
+        showPredictionPhrase()
+
+    else: 
+        showPredictionPhrase()
+        time_previous_char_first = time.time()
+        time_out = time.time()
+        if (prediction_char == 'del') : deleteChar()
+        else : 
+            if (time_out - time_in) > time_clear:
+                print("Clear")
+                predicted_phrase.clear()
+                previous_char = None
+        
     cv2.imshow("Frame", frame_rgb_flip)
 
 
